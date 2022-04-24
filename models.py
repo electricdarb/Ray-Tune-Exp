94a3add1d63d5b99c9076636fdf10e67c735901a
Bradford Gill <bradfordgill@umass.edu>
2022-04-24 15:18:32 -0400

    init

---
+++ models.py
@@ -0,0 +1,62 @@
+import torch 
+from torch import nn, xlogy, xlogy_
+from torch.nn import functional as F
+
+
+class SimpleCNN(nn.Module):
+    """Simple Cnn for binary classification"""
+
+    def __init__(self, image_size: int = 227, in_channels: int = 3, transforms = None, dropout_rate = .2) -> None:
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size = 3, padding = 'same')
+        self.conv2 = nn.Conv2d(32, 32, kernel_size = 3, padding = 'same')
+        self.pool1 = nn.MaxPool2d(kernel_size = 2) # down samples to imagesize // 2 :(113)
+
+        self.conv3 = nn.Conv2d(32, 64, kernel_size = 3, padding = 'same')
+        self.conv4 = nn.Conv2d(64, 64, kernel_size = 3, padding = 'same')
+        self.pool2 = nn.MaxPool2d(kernel_size = 2) # down samples to imagesize // 2 :(56x56x64)
+
+        self.conv5 = nn.Conv2d(64, 128, kernel_size = 3, padding = 'same')
+        self.conv6 = nn.Conv2d(128, 128, kernel_size = 3, padding = 'same')
+        self.pool3 = nn.MaxPool2d(kernel_size = 2) # down samples to imagesize // 2 :(28x28x128)
+
+        self.dropout = nn.Dropout(dropout_rate)
+
+        dense_dims = (image_size // 8) ** 2 * 128
+        self.dense = nn.Linear(dense_dims, 1)
+
+    def forward(self, input: torch.tensor) -> torch.tensor:
+        """forward pass through device"""
+        x = self.conv1(input)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.pool1(x)
+
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = self.conv4(x)
+        x = F.relu(x)
+        x = self.pool2(x)
+
+        x = self.conv5(x)
+        x = F.relu(x)
+        x = self.conv6(x)
+        x = F.relu(x)
+        x = self.pool3(x)
+
+        # flatten while preserving batch 
+        x = torch.reshape(x, (x.shape[0], -1))
+
+        # pass through a dense layer, mapping a binary from 0 to 1
+        x = self.dense(x)
+        x = torch.sigmoid(x)
+        x = torch.flatten(x)
+
+        return x
+
+
+
+
+
---
+++ train.py
@@ -0,0 +1,197 @@
+from tkinter import image_names
+from sklearn.utils import shuffle
+import torch 
+from torch import dropout, dropout_, nn
+import torchvision
+from torchvision import transforms, utils, datasets
+from torch.utils.data import Dataset, DataLoader
+from torch.nn import functional as F
+
+from models import SimpleCNN
+
+from tqdm import tqdm
+
+from filelock import FileLock
+import os 
+
+import ray
+import ray.tune as tune
+
+# create dataloaders
+def create_dataloader(path, transform, split = .1, batch_size = 64):
+    """inputs a path to an image folder and returns train_loader and val_loader"""
+    dataset = datasets.ImageFolder(root = path, transform = transform)
+
+    # split data
+    splits = [len(dataset) - int(len(dataset) * split), int(len(dataset) * split)]
+    train_set, val_set = torch.utils.data.random_split(dataset, splits)
+
+    # create loader
+    train_loader = DataLoader(train_set, batch_size = batch_size, shuffle = True)
+    val_loader = DataLoader(val_set, batch_size = batch_size, shuffle = True)
+
+    return train_loader, val_loader
+
+def train(config: dict) -> None:
+    """create and train a model, config is of structure:
+    config = {
+        lr:
+        l2:
+        momentum:
+        dropout_rate:
+        image_size:
+        epochs:
+        """
+
+    image_size = config['image_size']
+    lr = config['lr']
+    momentum = config['momentum']
+    l2 = config['l2']
+    dropout_rate = config['dropout_rate']
+    epochs = config['epochs']
+
+    # define device to use gpu if aviable 
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+    # create base transforms 
+    base_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize(image_size),
+        transforms.Normalize(mean = 0, std = 1),
+    ])
+
+    # create training transforms
+    train_tranforms = transforms.Compose([
+        transforms.RandomRotation(30),
+        transforms.RandomVerticalFlip(),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomAffine(0, translate = (.1, .1), scale = (.9, 1.1))
+    ])
+    # load in data in a thread safe manor
+    with FileLock(os.path.expanduser("~/data.lock")):
+        train_loader, val_loader = create_dataloader(path = 'data', transform = base_transforms)
+
+    # create model 
+    model = SimpleCNN(image_size = image_size, dropout_rate = dropout_rate)
+    model.to(device)
+
+    # create loss
+    loss_fn = F.binary_cross_entropy
+    
+    # create optimizer 
+    optimizer = torch.optim.SGD(
+        model.parameters(), 
+        lr = lr,
+        momentum = momentum,
+        weight_decay = l2)
+
+    # iter through epochs 
+    for epoch in range(epochs):
+
+        train_loss = 0
+        train_correct = 0
+        # iter through training batches 
+        for images, labels in tqdm(train_loader):
+            images, labels = images.to(device), labels.to(device).float()
+
+            # apply training trainsforms 
+            images = train_tranforms(images)
+
+            # run images through model and calculate loss 
+            outputs = model(images)
+            loss = loss_fn(outputs, labels)
+
+            # reset optimizer
+            optimizer.zero_grad()
+
+            # compute loss gradients and take a step
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item()
+
+            # sum where outputs are equal to correct labels
+            train_correct += (outputs == labels).float().mean()
+
+        # average accuracy over each batch
+        train_accuracy = train_accuracy / len(train_loader)
+
+        val_loss = 0
+        val_correct = 0
+
+        # iter through val batches 
+        with torch.no_grad():
+            for images, labels in val_loader:
+                images, labels = images.to(device), labels.to(device)
+
+                # run images through model and calculate loss 
+                outputs = model(images)
+                loss = loss_fn(outputs, labels)
+
+                val_loss += loss.item()
+
+                val_correct += (outputs == labels).float().mean()
+
+            # avergae accuracy 
+            val_accuracy = val_correct / len(val_loader)
+        
+        print(f'Epoch: {epoch + 1} / {epochs}:: {train_accuracy =}, {train_loss = }, {val_accuracy =}, {val_loss =}')
+
+        tune.report(
+            train_accuracy = train_accuracy,
+            train_loss = train_loss,
+            val_accuracy = val_accuracy,
+            val_loss = val_loss
+            )
+
+
+def hyp_search():
+    # define scheduler 
+    sched = AsyncHyperBandScheduler()
+
+    analysis = tune.run(
+        train,
+        metric = "val_accuracy",
+        mode = "max",
+        name = 'exp',
+        scheduler=sched,
+        stop={
+            "training_iteration": 20
+        },
+        resources_per_trial={"cpu": 4, "gpu": 1 if torch.cuda.is_available() else 0},  # set this for GPUs
+        num_samples = 5,
+        config = dict(
+            image_size = 227 // 2,
+            lr = tune.loguniform(1e-4, 1.),
+            momentum = tune.uniform(0.1, 0.9),
+            l2 = tune.loguniform(1e-4, 1e-2),
+            dropout_rate = tune.uniform(.05, .35)
+            epochs = 100000 # set to high number so iterations is used to stop not this
+            )
+    )
+
+    print("Best config is:", analysis.best_config)
+
+if __name__ == "__main__":
+    config = dict(
+        image_size = 227 // 2,
+        lr = .01,
+        momentum = .9,
+        l2 = 1e-4,
+        dropout_rate = .2,
+        epochs = 1
+    )
+
+    train(config)
+
+
+
+
+
+    
+    
+    
+
+# 
+
